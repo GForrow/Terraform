a97fb6df81007e77aef5ec316c631bd99cf6f8e9
Ubuntu <ubuntu@ip-172-31-38-243.eu-west-1.compute.internal>
2020-08-19 15:36:26 +0000

    terraform setup

--- main.tf
+++ main.tf
@@ -3,8 +3,86 @@ provider "aws" {
   shared_credentials_file = "~/.aws/credentials"
 }
 
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.cidr_block
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "VPC190820"
+  }
+
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "subnet_a" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.subnet_cidr_block
+  availability_zone = data.aws_availability_zones.available.names[0]
+  tags = {
+    Name = "TF_subnet"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "main_terraform_GW"
+  }
+}
+
+resource "aws_route_table" "r" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "main_terraform_RT"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet_a.id
+  route_table_id = aws_route_table.r.id
+}
+
+resource "aws_security_group" "sg_web" {
+  name        = var.sg_web_name
+  description = var.sg_web_description
+  vpc_id      = aws_vpc.vpc.id
+
+  dynamic "ingress" {
+    iterator = port
+    for_each = var.ingress_ports
+    content {
+      from_port   = port.value
+      protocol    = "tcp"
+      to_port     = port.value
+      cidr_blocks = [var.open_internet]
+    }
+  }
+
+  egress {
+    from_port   = var.outbound_port
+    protocol    = "-1"
+    to_port     = var.outbound_port
+    cidr_blocks = [var.open_internet]
+  }
+
+}
+
 resource "aws_instance" "web" {
-  ami           = var.ami
-  instance_type = var.instance
-  key_name      = var.key_name
+  ami                         = var.ami
+  instance_type               = var.instance
+  key_name                    = var.key_name
+  subnet_id                   = aws_subnet.subnet_a.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.sg_web.id]
 }
